9fa943d09d6c7b3ba57b8bbd05a66bc4ff8f8d4c
Your Name <you@example.com>
2022-06-12 18:46:26 +0530

    new modified

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "CI CD pipeline has been established."
+    return " new CI CD pipeline has been established."
 
 
 if __name__=="__main__":
